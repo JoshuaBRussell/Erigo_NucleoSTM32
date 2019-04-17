eb43a8fbdbfde8bab5ef942629c3ec112bb4a07d
JoshuaBRussell <theruss007@gmail.com>
2019-04-17 15:57:24 -0500

    Add function to send CMD, and wait for ACK from uC.

--- STM32_Serial.py
+++ STM32_Serial.py
@@ -14,6 +14,42 @@ TEST_AMPLITUDE_MA  = 300
 NM_AMPITUDE_MA = 80
 FREQ_SEL = 3 #0/1/2/3 <->12.5/25/50/100Hz  
 
+def wait_for_ACK(min_handler: MINTransportSerial, timeout = 0.1):
+    start = time.time()
+    while True:
+        frames = min_handler.poll()
+        if frames:
+            return frames
+        
+        #Return an empty frames after timeout
+        if time.time() - start >= timeout:
+            return frames
+
+def send_CMD(cmd):
+    """Sends a MIN frame with the CMD as the MIN identifier byte. Then waits for an ACK from the uC with the same CMD."""
+    #junk data at the moment
+    data = b''
+    data_array = [100, 50, 3]
+    for i in range(len(data_array)):
+        data+=data_array[i].to_bytes(2, byteorder="big")
+    
+    #Send CMD
+    print("Send CMD...")
+    min_handler.send_frame(cmd, data)
+    
+    #Wait for ACK from uC
+    print("Waiting for ACK...")
+    frames = wait_for_ACK(min_handler, timeout = 1.0)
+
+    #There should only be one frame, but just in case/wait for ACK returns a lit of frames
+    for frame in frames:
+        if frame.min_id == cmd:
+            print("Success!!!")
+            return True
+        else:   
+            print("Failure")
+            return False
+
 def main(Test_Amplitude_mA, NM_Amplitude_mA, Freq_Sel):
     ser = serial.Serial("COM3", 115200, timeout = 1.0)
     for i in range(5):
@@ -36,13 +72,17 @@ if __name__ == "__main__":
     #main(TEST_AMPLITUDE_MA, NM_AMPITUDE_MA, FREQ_SEL)
 
     min_handler = MINTransportSerial("COM3", 115200)
-    data_array = [100, 50, 0]
-    for i in range(SEND_ATTEMPS):
-        data = b''
-        for i in range(len(data_array)):
-            data+=data_array[i].to_bytes(2, byteorder="big")
-        min_handler.send_frame(1, data)
-        print(data)
-        time.sleep(1)
+    
+    CMD = 2
+    COMM_SUCC = False
+    while(not(COMM_SUCC)):
+        COMM_SUCC = send_CMD(CMD)
+
+    print("Comm was a Succ")
+
+
+    
+
+
 
     
