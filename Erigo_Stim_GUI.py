d7126e4c34181e18c1a9849d30617ea9c99cc531
JoshuaBRussell <theruss007@gmail.com>
2019-05-29 14:22:54 -0500

    Add ADC Threshold Entry to GUI. Do some minor data processing on returned data to show MAX, MIN, and Rec ADC Threshold to send to uC.

--- Erigo_Stim_GUI.py
+++ Erigo_Stim_GUI.py
@@ -62,7 +62,7 @@ def Send_Get_Pos_Data_Command():
 
     #data collected from uC
     theta = np.arange(0.0, 1000.0, 1.0)
-    y = 750.0*np.sin((0.1/np.pi)*theta)+3000.0
+    y = 750.0*np.sin((0.1/np.pi)*theta)+3000.0 + np.random.normal(0.0, 25.0, theta.size)
     data = np.vstack((theta, y))
     print("Gathered the data...")
     print("Data shape: ", data.shape)
@@ -72,9 +72,16 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "ERIGO Stimulation Control", font = LARGE_FONT)
-        label.grid(row=0,column=0, sticky = 'N', columnspan = 2) #Stick forces text to be "North". columnspan causes the text to bridge 
+        self.title = tk.Label(self, text = "ERIGO Stimulation Control", font = LARGE_FONT)
+        self.title.grid(row=0,column=0, sticky = 'N', columnspan = 2) #Stick forces text to be "North". columnspan causes the text to bridge 
                                                                  #the columns used for the label/entry pairs
+
+        self.ADC_Stats_Max = tk.StringVar()
+        self.ADC_Stats_Max.set("Max ADC Pos: --")
+        self.ADC_Stats_Min = tk.StringVar()
+        self.ADC_Stats_Min.set("Min ADC Pos: --")
+        self.ADC_Stats_Rec = tk.StringVar()
+        self.ADC_Stats_Rec.set("Rec ADC Threshold: --")
 
         #Create Label/Entry Objects
         NM_AMP_Entry_Label = tk.Label(self, text = "NM AMP Selection:")
@@ -83,6 +90,8 @@ class StartPage(tk.Frame):
         Diag_AMP_Entry = tk.Entry(self)
         Freq_Entry_Label = tk.Label(self, text = "Freq Selection:")
         Freq_Entry = tk.Entry(self)
+        ADC_Threshold_Entry_Label = tk.Label(self, text = "ADC Threshold:")
+        ADC_Threshold_Entry = tk.Entry(self)
 
         #Place them in a grid pattern
         NM_AMP_Entry_Label.grid(row=1, column = 0)
@@ -91,20 +100,31 @@ class StartPage(tk.Frame):
         Diag_AMP_Entry.grid(row=2, column=1)
         Freq_Entry_Label.grid(row=3, column = 0)
         Freq_Entry.grid(row=3, column=1)
+        ADC_Threshold_Entry_Label.grid(row=4, column = 0)
+        ADC_Threshold_Entry.grid(row=4, column=1)
 
         #Insert default values
         NM_AMP_Entry.insert(0, '0')
         Diag_AMP_Entry.insert(0, '0')
         Freq_Entry.insert(0, '0')
+        ADC_Threshold_Entry.insert(0, '0')
 
         Stim_Button = tk.Button(self, text = "Start Stim", command = lambda: Send_Stim_Command(NM_AMP_Entry.get(), Diag_AMP_Entry.get(), Freq_Entry.get()))
-        Stim_Button.grid(row=4, column=0)
+        Stim_Button.grid(row=5, column=0)
 
         Stop_Stim_Button = tk.Button(self, text = "Stop Stim", command = lambda: Send_Stop_Stim_Command())
-        Stop_Stim_Button.grid(row = 4, column = 1)
+        Stop_Stim_Button.grid(row = 5, column = 1)
 
-        Record_Data_Button = tk.Button(self, text = "Get Position Data", command = lambda: self.Update_Plot(canvas, angle_plot))
-        Record_Data_Button.grid(row= 4, column=3)
+        Record_Data_Button = tk.Button(self, text = "Get Position Data", command = lambda: self.Update_ADC_Pos(canvas, angle_plot))
+        Record_Data_Button.grid(row= 5, column=4)
+
+        self.Max_ADC_Label = tk.Label(self, textvariable = self.ADC_Stats_Max)
+        self.Rec_ADC_Label = tk.Label(self, textvariable = self.ADC_Stats_Rec)
+        self.Min_ADC_Label = tk.Label(self, textvariable = self.ADC_Stats_Min)
+
+        self.Max_ADC_Label.grid(row = 3, column=3)
+        self.Rec_ADC_Label.grid(row = 3, column=4)
+        self.Min_ADC_Label.grid(row = 3, column=5)
 
         #Creates a matplotlib Figure and a subplot on it
         Fig = Figure(figsize=(4,4), dpi=100)
@@ -116,9 +136,9 @@ class StartPage(tk.Frame):
         #Canvas is a drawing area for tkinter. We put the matplotlib figure onto the canvas
         canvas = FigureCanvasTkAgg(Fig, self)
         canvas.show()
-        canvas.get_tk_widget().grid(row=0, column=3)
+        canvas.get_tk_widget().grid(row=0, column=3, columnspan = 3)
 
-    def Update_Plot(self, canvas, plot_obj):
+    def Update_ADC_Pos(self, canvas, plot_obj):
 
         data = Send_Get_Pos_Data_Command()
         
@@ -127,11 +147,32 @@ class StartPage(tk.Frame):
         y_data = data[1, :]
         
         plot_obj.clear()
+
+        #Scatter Plot for Raw Data
         plot_obj.scatter(x_data, y_data, color="blue")
         plot_obj.set_xlim([PLOT_LWR_LIMIT_X, PLOT_UPR_LIMIT_X])
         plot_obj.set_ylim([PLOT_LWR_LIMIT_Y, PLOT_UPR_LIMIT_Y])
         #plot_obj.set_xlim([0.0, 1000.0])
         #plot_obj.set_ylim([min(y_data), max(y_data)])
+
+        #Processing on Data (if any more data processing were to be done, I would consider making it a seperate module)
+        max_90_percentile = np.percentile(y_data, 90.0)
+        min_10_percentile = np.percentile(y_data, 10.0)
+        mid_50_percentile = np.percentile(y_data, 50.0)
+        AVG = (max_90_percentile+min_10_percentile)/2
+
+        print("MAX: ", max_90_percentile)
+        print("AVG: ", AVG)
+        print("MIN: ", min_10_percentile)
+        print("MID: ", mid_50_percentile)
+
+        self.ADC_Stats_Max.set("Max ADC Pos: " + str(int(max_90_percentile)))
+        self.ADC_Stats_Min.set("Min ADC Pos: " + str(int(min_10_percentile)))
+        self.ADC_Stats_Rec.set("Rec ADC Threshold: " + str(int(AVG)))
+
+        plot_obj.hlines(max_90_percentile, PLOT_LWR_LIMIT_X, PLOT_UPR_LIMIT_X)
+        plot_obj.hlines(min_10_percentile, PLOT_LWR_LIMIT_X, PLOT_UPR_LIMIT_X)
+        plot_obj.hlines(AVG, PLOT_LWR_LIMIT_X, PLOT_UPR_LIMIT_X, colors = "red")
 
         canvas.draw()
 
